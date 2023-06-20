fabfd781cd900e8b1695851bb9a41490d8d6d416
Ratnesh <ratneshjain40@gmail.com>
2023-06-20 18:46:40 +0530

    Custom Contact validator

--- src/dtos/otp.dto.ts
+++ src/dtos/otp.dto.ts
@@ -1,16 +1,26 @@
-import { IsEmail, IsEnum, IsString, ValidateIf } from 'class-validator';
+import { IsEnum, IsString, Validate, ValidationArguments, ValidatorConstraint, ValidatorConstraintInterface } from 'class-validator';
 
 export enum otpType {
   EMAIL = 'EMAIL',
   MOBILE = 'MOBILE',
 }
 
-export class SendOtpDto {
-  @IsEmail()
-  @ValidateIf((o) => o.type === otpType.EMAIL)
-  public contact: string;
+@ValidatorConstraint({ name: 'validateContact', async: false })
+class ValidateContact implements ValidatorConstraintInterface {
+  validate(text: string, args: ValidationArguments) {
+    const type = args.object['type'];
+    if (type === otpType.EMAIL) {
+      const regex = new RegExp(/^[a-zA-Z0-9+_.-]+@[a-zA-Z0-9.-]+$/);
+      return regex.test(text);
+    } else if (type === otpType.MOBILE) {
+      const regex = new RegExp(/^(\+91)[0-9]{10}$/);
+      return regex.test(text);
+    }
+  }
+}
 
-  @ValidateIf((o) => o.type === otpType.MOBILE)
+export class SendOtpDto {
+  @Validate(ValidateContact, { message: 'Invalid contact' })
   public contact: string;
 
   @IsEnum(otpType)
