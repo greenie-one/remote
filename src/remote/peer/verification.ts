c03cf0f771f21fe36d6bdb55b87964695a7e41a1
dharmikjethva30b <dharmikjethva30@gmail.com>
2023-07-16 10:53:35 +0530

    added enpoints for sending peer verification

---
+++ src/remote/peer/verification.ts
@@ -0,0 +1,37 @@
+import { env } from '@/config';
+import ejs from 'ejs';
+import { mailer } from '../generic/emailer';
+import { HttpClient } from '../generic/httpClient';
+
+const ACCOUNT_SID = env('TWILIO_ACCOUNT_SID');
+const AUTH_TOKEN = env('TWILIO_AUTH_TOKEN');
+const FROM_MOBILE = env('TWILIO_FROM_MOBILE');
+
+export class verfication {
+  static async sendMail(firstName: string, userName: string, email: string, verificationLink: string) {
+    const html = await ejs.renderFile('templates/email/peerVerfication.ejs', { firstName, userName, verificationLink });
+    return mailer.sendMail({
+      to: email,
+      subject: 'Verify Work Experience',
+      html,
+    });
+  }
+
+  static async requestOnMobile(firstName: string, userName: string, phone: string, verificationLink: string) {
+    const body = await ejs.renderFile('templates/sms/verificationTemplate.ejs', { firstName, userName, verificationLink });
+    await HttpClient.callApi({
+      url: `https://api.twilio.com/2010-04-01/Accounts/${ACCOUNT_SID}/Messages.json`,
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/x-www-form-urlencoded',
+        Authorization: `Basic ${Buffer.from(`${ACCOUNT_SID}:${AUTH_TOKEN}`).toString('base64')}`,
+      },
+      body: {
+        To: phone,
+        From: FROM_MOBILE,
+        Body: body,
+      },
+    });
+    return true;
+  }
+}
