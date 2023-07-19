e92275484f0f433bbda705ed3cd40f0bd53b8b3a
Ratnesh <ratneshjain40@gmail.com>
2023-07-19 16:31:43 +0530

    validate mobile

--- src/dtos/verification.dto.ts
+++ src/dtos/verification.dto.ts
@@ -1,10 +1,19 @@
-import { IsString } from 'class-validator';
+import { IsString, Validate, ValidatorConstraint, ValidatorConstraintInterface } from 'class-validator';
+
+@ValidatorConstraint({ name: 'validateMobile', async: false })
+class ValidateMobile implements ValidatorConstraintInterface {
+  validate(text: string) {
+    const regex = new RegExp(/^(\+91)[0-9]{10}$/);
+    return regex.test(text);
+  }
+}
 
 export class SendPeerLinkDTO {
   @IsString()
   public email: string;
 
   @IsString()
+  @Validate(ValidateMobile, { message: 'Invalid mobile number' })
   public phone: string;
 
   @IsString()
