0a462c4b3e9f71d6f6c29b80d4f8d1ba486fae5a
Sahil Gupte <ovenoboyo@gmail.com>
2023-07-27 17:11:32 +0530

    Generate and return otp

--- src/services/otp.service.ts
+++ src/services/otp.service.ts
@@ -1,20 +1,24 @@
 import { SendOtpDto } from '@/dtos/otp.dto';
-import { HttpException } from '@/exceptions/httpException';
 import { ErrorEnum } from '@/exceptions/errorCodes';
+import { HttpException } from '@/exceptions/httpException';
 import { AuthRemote } from '@/remote/auth/otp.remote';
+import { generateRandomNumber } from '@/utils/otp';
 
 class sendOtpService {
   public async sendOtp(sendOtpDto: SendOtpDto) {
+    const otp = generateRandomNumber().toString()
     try {
       if (sendOtpDto.type === 'EMAIL') {
-        await AuthRemote.requestOtpEmail(sendOtpDto.contact, sendOtpDto.otp);
+        await AuthRemote.requestOtpEmail(sendOtpDto.contact, otp);
         return { message: 'OTP sent successfully' };
       }
 
       if (sendOtpDto.type === 'MOBILE') {
-        await AuthRemote.requestOtpMobile(sendOtpDto.contact, sendOtpDto.otp);
+        await AuthRemote.requestOtpMobile(sendOtpDto.contact, otp);
         return { message: 'OTP sent successfully' };
       }
+
+      return { otp }
     } catch (error) {
       throw new HttpException(ErrorEnum.OTP_NOT_SENT);
     }
